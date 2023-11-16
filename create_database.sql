f58542a93658995086209555e2f179475400054d
Vaios Christodoulou <vallos1501@gmail.com>
2023-11-16 15:21:02 +0200

    updated data types

--- create_database.sql
+++ create_database.sql
@@ -1,50 +1,50 @@
 CREATE TABLE user (
     id              INT AUTO_INCREMENT PRIMARY KEY,
-    name            TEXT,
-    date_of_birth   TIMESTAMP,
-    username        TEXT NOT NULL,
-    password        TEXT NOT NULL,
+    name            VARCHAR(255) NOT NULL,
+    date_of_birth   DATE,
+    username        VARCHAR(50) NOT NULL,
+    password        VARCHAR(255) NOT NULL,
     email           VARCHAR(255) UNIQUE NOT NULL,
-    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
 CREATE TABLE user_status (
     id          INT AUTO_INCREMENT PRIMARY KEY,
     user_id     INT,
     type        ENUM('EMPLOYEE', 'EMPLOYER'),
-    rating      INT,
+    rating      DECIMAL(2, 1),
     FOREIGN KEY (user_id) REFERENCES user(id)
 );
 
 CREATE TABLE job_listing (
     id                  INT AUTO_INCREMENT PRIMARY KEY,
     employer_user_id    INT,
-    job_title           TEXT,
+    job_title           VARCHAR(50),
     job_description     TEXT,
     experience_level    ENUM('BEGINNER', 'INTERMEDIATE', 'EXPERT'),
     status              ENUM('NEW', 'ONGOING', 'DONE'),
     start_date          TIMESTAMP,
     end_date            TIMESTAMP,
-    location            TEXT,
-    reward              INT,
+    location            VARCHAR(255),
+    reward              DECIMAL(10, 2),
     FOREIGN KEY (employer_user_id) REFERENCES user(id),
 );
 
 CREATE TABLE category (
-    id              INT AUTO_INCREMENT PRIMARY KEY,
-    category_name   VARCHAR(255) UNIQUE NOT NULL,
+    category_name   VARCHAR(255) UNIQUE NOT NULL PRIMARY KEY,
     description     TEXT
 );
 
 CREATE TABLE skill (
-    id          INT AUTO_INCREMENT PRIMARY KEY,
-    category_id INT,
-    name        TEXT,
-    description TEXT,
+    id              INT AUTO_INCREMENT PRIMARY KEY,
+    category_id     INT,
+    name            VARCHAR(50),
+    description     TEXT,
     FOREIGN KEY (category_id) REFERENCES category(id)
 );
 
 CREATE TABLE user_skill (
+    id          INT AUTO_INCREMENT PRIMARY KEY,
     skill_id    INT,
     user_id     INT,
     FOREIGN KEY (skill_id) REFERENCES skill(id),
@@ -53,7 +53,7 @@ CREATE TABLE user_skill (
 
 CREATE TABLE job_listing_category (
     id                  INT AUTO_INCREMENT PRIMARY KEY,
-    job_listing_id          INT,
+    job_listing_id      INT,
     category_id         INT,
     FOREIGN KEY (job_listing_id) REFERENCES job_listing(id),
     FOREIGN KEY (category_id) REFERENCES category(id)
