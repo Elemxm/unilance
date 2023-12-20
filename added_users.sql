d85c01d81c2bef5b8fca915a3de20fd2d35af264
Elemxm <elenamaxmoutai@gmail.com>
2023-12-20 23:45:52 +0200

    Added users to database

---
+++ added_users.sql
@@ -0,0 +1,17 @@
+-- Single User (Employee/Company)
+CREATE USER 'singleuser'@'%' IDENTIFIED BY 'password123';
+GRANT CREATE, SELECT ON student.* TO 'singleuser'@'%';
+
+-- Employee
+CREATE USER 'employeeuser'@'%' IDENTIFIED BY 'password456';
+GRANT CREATE, SELECT, INSERT ON student.* TO 'employeeuser'@'%';
+
+-- Company
+CREATE USER 'companyuser'@'%' IDENTIFIED BY 'password789';
+GRANT CREATE, SELECT, INSERT ON student.* TO 'companyuser'@'%';
+
+-- Admin
+CREATE USER 'adminuser'@'localhost' IDENTIFIED BY 'adminpassword';
+GRANT CREATE, DROP, ALTER ON student.* TO 'adminuser'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON student.* TO 'adminuser'@'localhost';
+-- DROP USER 'adminuser'@'localhost';
