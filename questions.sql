a499f335d5a7018be76a05ceae86dacc59a468d5
Vaios Chrsitodoulou <vallos1501@gmail.com>
2023-12-21 10:40:30 +0200

    corrected 3rd question

--- questions.sql
+++ questions.sql
@@ -18,8 +18,9 @@ WHERE
 	rating_user.id = 1;
 
 -- 3
-SELECT *
+SELECT user.username, user.email, application.created_date AS application_date
 FROM application JOIN employee ON application.employee_id = employee.id
+	JOIN user ON employee.user_id = user.id
 WHERE application.job_listing_id = 4;
 
 -- 4
